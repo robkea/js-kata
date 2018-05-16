0c7c00c7daf6b40bd46573ac2d01775402cdef21
Robert Alexandru Tutu <robe3046@stud.kea.dk>
2018-05-16 22:39:53 +0200

    Additional fun

--- w20tuesday/mysolution/mockify.js
+++ w20tuesday/mysolution/mockify.js
@@ -17,3 +17,27 @@ function mockify(str) {
     }
     return str = str.join(``);
 }
+
+
+//  Modify the function, so that it uses this as input-parameter, if nothing else is supplied (i.e. a default parameter)
+// In the console, write: String.prototype.mockify = mockify;
+// Now you will be able to mockify any string - try "This is a test".mockify() for instance.
+// You can also try document.querySelector('h1').textContent = document.querySelector('h1').textContent.mockify()
+
+String.prototype.mockifyThis = function mockifyThis() {
+    let str = Array.from(this);
+
+    for(let i = 0; i < str.length; i++) {
+     let randomNumber = Math.round( Math.random() );   
+     
+     if( randomNumber == 0) {
+         str[i] = str[i].toUpperCase();
+     }
+     else {
+         str[i] = str[i].toLowerCase();
+     }
+    }
+   return str = str.join('');
+}
+
+document.querySelector('h1').textContent = document.querySelector('h1').textContent.mockifyThis()
